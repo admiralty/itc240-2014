62fb376ebabf32bfa28c9eaa4b7167ba06b5508f
Chris Moreno <moreno.christopher@gmail.com>
2014-04-15 16:37:11 -0700

    updated changes

--- assignment-1/index.php
+++ assignment-1/index.php
@@ -1,3 +1,12 @@
-<!doctype html>
+<?php
+$page_title = "Index page";
+include("header.inc");
+?>
+
+Earth people. New York and California. Earth people. I was born on 
+Jupiter.
+
+
+//<!doctype html>
 <html>
 Greetings earth people!
