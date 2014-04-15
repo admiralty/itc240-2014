906481e79461c5eae8c288bdf5fd12c5658d8c03
Chris Moreno <moreno.christopher@gmail.com>
2014-04-15 16:54:08 -0700

    the footer

---
+++ assignment-1/footer.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+  <footer>
+    <title><?php echo $page_title; ?></title>
+  </footer>
+  <body>
+    <h1>CONGRATULATIONS! THE FINISH LINE IS SOMEWHERE IN THE BONAVENTURE 
+HOTEL.</h1>
+
+<?php
+echo $page_title;
+?>
