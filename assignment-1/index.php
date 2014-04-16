1430ffd85ccfaf80790b687ecd2cb2964c7621ca
Chris Moreno <moreno.christopher@gmail.com>
2014-04-15 17:42:26 -0700

    newest brightest index page

--- assignment-1/index.php
+++ assignment-1/index.php
@@ -3,10 +3,13 @@ $page_title = "Earth people!";
 include("header.php");
 ?>
 
-New York and California. Earth people! I was born on Jupiter. Art 
-avenger, let's start the adventure. Super-sleuth, a new race. Mad creator, savage nature. 
+New York and California. Earth people! I was born on Jupiter.<br><br> 
+Art 
+avenger, let's start the adventure. Super-sleuth, a new race. Mad 
+creator, savage nature.<br><br> 
 World Wide Web, the ebb and flow. Light years from watchful eyes while 
-my thoughts provide objectives. Asteroid surfing, drift by a star, 
+my thoughts provide objectives.<br><br> Asteroid surfing, drift by a 
+star, 
 absorb it, and store it.
 
 <?php
