7e76bce1eb7a0369ddaa9d2e6fe80158b21a2ad5
Chris Moreno <moreno.christopher@gmail.com>
2014-04-15 17:27:20 -0700

    the footer

--- assignment-1/footer.php
+++ assignment-1/footer.php
@@ -4,9 +4,9 @@
     <title><?php echo $page_title; ?></title>
   </footer>
   <body>
-    <h1>CONGRATULATIONS! THE FINISH LINE IS SOMEWHERE IN THE BONAVENTURE 
-HOTEL.</h1>
+    <h1>CONGRATULATIONS! The finish line is somewhere...in the 
+Bonaventure Hotel.</h1>
 
 <?php
-echo $page_title;
+//echo $page_title;
 ?>
