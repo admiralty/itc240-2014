0d491b2973f35ef79b842011fc2005e142ecf49e
Chris Moreno <moreno.christopher@gmail.com>
2014-04-15 17:15:54 -0700

    newest biggest changes

--- assignment-1/index.php
+++ assignment-1/index.php
@@ -1,9 +1,13 @@
 <?php
 $page_title = "Earth people!";
-include("header.inc");
+include("header.php");
 ?>
 
-New York and California.
+New York and California. Earth people! I was born on Jupiter. Art 
+avenger, let's start the adventure. Super-sleuth, a new race. Mad creator, savage nature. 
+World Wide Web, the ebb and flow. Light years from watchful eyes while 
+my thoughts provide objectives. Asteroid surfing, drift by a star, 
+absorb it, and store it.
 
 <?php
 include("footer.php");
