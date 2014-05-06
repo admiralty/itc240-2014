74ffc6693b10de8a31fc6d62a6c49278010e7764
Chris Moreno <moreno.christopher@gmail.com>
2014-05-06 02:54:58 -0700

    alphabetical

---
+++ assignment-3/alphabetical.php
@@ -0,0 +1,5 @@
+<ul type="circle">
+    <li><b><?= $park["name"] ?></b></li>
+    <li><i>Neighborhood:</i> <?= $park["neighborhood"] ?></li>
+    <li><i>Rating:</i> <?= $park["rating"] ?></li>
+</ul>
