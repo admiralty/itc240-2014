673149c87b7e9038b7584fbdd19fe02932a31852
Chris Moreno <moreno.christopher@gmail.com>
2014-05-20 18:53:59 -0700

    First MySQL

--- assignment-4/index.php
+++ assignment-4/index.php
@@ -1,4 +1,49 @@
+<!doctype html>
+<html>
+        <body>
+                <h1>Seattle Parks</h1>  
+                <h3>(sorted by highest rated)</h3>
+                        
 <?php
 
 include("password.php");
 
+$mysql = new mysqli("localhost", "cmoren03", $mysql_password, "cmoren03");
+
+//*work on this part*
+$result = $mysql->query('SELECT * FROM parks ORDER BY Rating DESC;');
+
+$query = $mysql->prepare("SELECT * FROM parks;");
+
+$query->execute();
+
+?>
+
+<table>
+    <div>
+        <tr>
+        <td><b><u>Name</u></b>&nbsp;
+        <td><b><u>Neighborhood</u></b>&nbsp;
+        <td><b><u>Rating out 10</u></b>&nbsp;
+        <td><b><u>Review</u></b>&nbsp;
+        
+    </div>
+        
+<?php
+foreach ($result as $row) {
+?>
+    
+            
+    <tr>
+        <td><b><?= $row["Name"] ?></b>
+        <td><?= $row["Neighborhood"] ?>
+        <td><?= $row["Rating"] ?>
+        <td><?= $row["Review"] ?>
+        
+<?php
+}
+?>
+</table>
+    
+        </body>
+</html>
