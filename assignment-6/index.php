e687a995b8ab88d24ad9da2d3d31d1e46c52cfb6
Chris Moreno <moreno.christopher@gmail.com>
2014-06-03 18:16:46 -0700

    the neko index

---
+++ assignment-6/index.php
@@ -0,0 +1,101 @@
+<!doctype html>
+
+<html>
+
+    <head>
+
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        
+    </head>
+    
+    <body>
+
+        <h1>&#x1f431; ねこのフィットネスとカロリーログ</h1>
+        
+        <h1>Neko's fitness log and calorie tracker</h1>
+    
+        
+        <form method="POST" action="update.php">
+            <input placeholder="Add an activity" name="kind">
+            <input placeholder="Add calories burned" name="calories">
+            <input type="submit" value="<< Submit Entries">
+        </form>
+        
+        <div>________________________________________________________</div>
+        
+<?php
+
+include("password.php");
+$mysql = new mysqli("localhost", "cmoren03", $mysql_password, "cmoren03");
+
+$column = 'date';
+if (isset($_REQUEST["sort"])){
+    $column = $_REQUEST["sort"];
+}
+
+$whitelist = ["date" => true, "activity" => true, "calories" => true, "id" => true];
+if (!isset($whitelist[$column])) {
+$column = 'date';
+}
+
+$prepared = $mysql->prepare("SELECT * FROM neko_punch ORDER BY $column DESC;");
+
+$prepared->execute();
+$results = $prepared->get_result();
+        
+foreach ($results as $row) {
+?>
+        
+<!--------
+<div>
+    <?=htmlentities($row["activity"]) ?>
+        
+        test test test
+        
+    <?=htmlentities($row["calories"]) ?>
+</div>
+--------->    
+    
+<?php
+}
+
+$sumQuery = $mysql->prepare('SELECT SUM(calories) AS sum FROM neko_punch;');
+$sumQuery->execute();
+$sumResult = $sumQuery->get_result();
+$sum = $sumResult->fetch_array();
+
+$maxQuery = $mysql->prepare('SELECT MAX(calories) AS calories FROM neko_punch;');
+$maxQuery->execute();
+$maxResult = $maxQuery->get_result();
+$max = $maxResult->fetch_array();
+
+?>        
+        
+<br><table width="30%" cellpadding="7">
+    
+    <div>
+    
+  <tr>
+    <th align="left"><u>Date of entry</u></th>
+    <th align="left"><u>Fitness activity</u></th>
+    <th align="left"><u>Calories burned</u></th>
+  </tr>    
+    
+    </div>
+    
+  <tr>
+    <td><?= htmlentities ($row["date"]) ?>
+    <td><?= htmlentities ($row["activity"]) ?>
+    <td><?= htmlentities ($row["calories"]) ?>
+        
+</table>
+        
+        <div>________________________________________________________</div>
+        <br><div><b>Total calories burned -- </b><?=$sum["sum"] ?></div>
+        <br><div><b>Most calories burned in one activity -- </b><?= $max["calories"] ?></div>
+        
+    </body>
+
+</html>
+
+
