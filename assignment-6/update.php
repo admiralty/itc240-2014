9c06f226c78d7aa1918b1b4c01d1ac6a89ae5f51
Chris Moreno <moreno.christopher@gmail.com>
2014-06-03 18:18:29 -0700

    the neko files

---
+++ assignment-6/update.php
@@ -0,0 +1,13 @@
+<?php
+
+include("password.php");
+
+$mysql = new mysqli("localhost", "cmoren03", $mysql_password, "cmoren03");
+
+$query = 'INSERT INTO neko_punch (accomplished_on, activity, calories) VALUES (NOW(), ?, ?);';
+$prepared = $mysql->prepare($query);
+$prepared->bind_param("si", $_REQUEST["activity"], $_REQUEST["calories"]);
+$prepared->execute();
+
+header("Location: index.php");
+
