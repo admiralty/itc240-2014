2c751d7228c53fff9b6bccd2c6bf2b6ae45a641e
Chris Moreno <moreno.christopher@gmail.com>
2014-06-17 01:10:52 -0700

    updated index page

--- assignment-8/index.php
+++ assignment-8/index.php
@@ -1,29 +1,42 @@
 <?php
 
-//$parks = [
+//$park_list = [
 //];
+
+include("header.php");
      
-include("page.php");
+//$park_list = $mysql->query('SELECT * FROM parks ORDER BY Rating DESC;');
+//$park_query = $mysql->prepare("SELECT * FROM parks;");
+//$park_query->execute();
+//print_r($park_list); //KEEP THIS COMMENTED OUT EVEN IF REMOVE OTHER COMMENTS
 
-$park_list = $mysql->query('SELECT * FROM parks ORDER BY Rating DESC;');
-$park_query = $mysql->prepare("SELECT * FROM parks;");
-$park_query->execute();
-//print_r($park_list);
+$main_info = 'Rating';
+if (isset($_REQUEST["sort"])){
+    $main_info = $_REQUEST["sort"];
+}
+$main_info = $mysql->real_escape_string($main_info);
 
+$whitelist = ["Name" => true, "Neighborhood" => true, "Rating" => true, "Review" => true];
+
+if (!isset($whitelist[$main_info])) {
+    $main_info = 'Rating';
+}
+
+$prepared = $mysql->prepare("SELECT * FROM parks ORDER BY Rating DESC;");
+$prepared->execute();
+$park_results = $prepared->get_result();
+
+foreach ($park_results as $info) {
 ?>
 
-	
-<?php
-foreach ($park_list as $row) {
-?>
-
-	<ul type="circle">
-		<li><b><?= $row["Rating"] ?>/10 -- </b><?= $row["Name"] ?></li>
-		<li><i>Neighborhood : </i><?= $row["Neighborhood"] ?></li>
-		<li><i>Review : </i><?= $row["Review"] ?></li>
+    <ul type="circle">
+		<li><b><?= htmlentities ($info["Rating"]) ?>/10</b></li>
+        <li><?= htmlentities ($info["Name"]) ?></li>
+		<li><i>Neighborhood : </i><?= htmlentities ($info["Neighborhood"]) ?></li>
+		<li><i>Review : </i><?= htmlentities ($info["Review"]) ?></li>
+        <li><a href="?update=<?= $row["ID"] ?>">Edit</a>
+        <li><a href="?delete=<?= $row["ID"] ?>">Delete</a>
 	</ul>
-		
-        
 <?php
 }
 ?>
