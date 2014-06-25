4d3b89bc9a4590db3ef2b6dfd4d554728da83824
Chris Moreno <moreno.christopher@gmail.com>
2014-06-24 19:56:44 -0700

    Speed works WOW

--- assignment-9/bus.php
+++ assignment-9/bus.php
@@ -18,21 +18,78 @@ class Bus {
 
 $bus = new Bus();
 
-if ($bus->armed ==false) {
-	echo "Speed<br>";
+if ($bus->armed == false) {
+	echo "<b>The Movie Speed in PHP</b><br><br>";
 }
 
-$bust_.setSpeed(60);
+//$bust_.setSpeed(65);
+$bus->setSpeed(65);
 if ($bus->armed == true) {
-	echo "Speed 1<br>";
+	echo "Speed OK so far<br>";
 }
 
-$bust_.setSpeed(80);
+$bus->setSpeed(75);
+if ($bus->armed == true) {
+	echo "Speed OK<br>";
+}
+
+$bus->setSpeed(85);
+if ($bus->armed == true) {
+	echo "Speed still OK<br>";
+}
+
+$bus->setSpeed(95);
+if ($bus->armed == true) {
+	echo "High speed but OK<br>";
+}
+
+$bus->setSpeed(105);
+if ($bus->armed == true) {
+	echo "Ludicrous speed but OK<br>";
+}
+
+$bus->setSpeed(55);
+if ($bus->armed == true) {
+	echo "Losing speed but OK<br>";
+}
+
+$bus->setSpeed(51);
+if ($bus->armed == true) {
+	echo "Getting dangerously slow<br>";
+}
+
+$bus->setSpeed(51);
+if ($bus->armed == true) {
+	echo "Lost too much speed - Movie over.<br>";
+}
+/*
+//$bust_.setSpeed(85);
+$bus->setSpeed(85);
 if ($bus->armed == true) {
 	echo "Speed 2<br>";
 }
 
-$bust_.setSpeed(20);
+//$bust_.setSpeed(105);
+$bus->setSpeed(105)
 if ($bus->armed == true) {
 	echo "Speed 3<br>";
 }
+
+//$bust_.setSpeed(25);
+$bus->setSpeed(25);
+if ($bus->armed == true) {
+	echo "Speed 4<br>";
+}
+
+//function eq() {
+     return $this->bus;
+   //}
+ //}
+
+//if ($bus->armed == true) {
+	//echo "Movie over<br>";
+//}
+
+$bus->speed();
+*/
+?>
